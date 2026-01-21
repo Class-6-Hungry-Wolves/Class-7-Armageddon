056593cd98042faad437442dab7c4c7661896cfb
NichFos <nicholasdfoster645@gmail.com>
2026-01-21 09:27:15 -0600

    Added following outputs for ease of viewing: EC2 and RDS security group ids, ec2 public ip, cloudwatch sns topic

--- lab-1/outputs.tf
+++ lab-1/outputs.tf
@@ -11,21 +11,30 @@ output "lab_1a_private_subnet_ids" {
   value = [for i in aws_subnet.lab-1a-database-subnet : i.id]
 }
 
+
+output "lab1_ec2_security_group_id" {
+  value = aws_security_group.lab_1a_ec2_sg.id
+}
+
+output "lab1_rds_security_group_id" {
+  value = aws_security_group.lab_1a_rds_sg.id
+}
+
 output "lab1_ec2_instance_id" {
   value = aws_instance.lab1_ec2_instance.id
 }
 
 output "lab1_ec2_public_ip" {
-  value = aws_instance.lab1_ec2_instance.public_ip
+  value = "http://${aws_instance.lab1_ec2_instance.public_ip}/init"
 }
 output "lab1_rds_endpoint" {
   value = aws_db_instance.lab1-rds01.address
 }
 
-# output "chewbacca_sns_topic_arn" {
-#   value = aws_sns_topic.chewbacca_sns_topic01.arn
-# }
+output "cloudwatch_sns_topic_arn" {
+  value = aws_sns_topic.armageddon_sns_topic01.arn
+}
 
-# output "chewbacca_log_group_name" {
-#   value = aws_cloudwatch_log_group.chewbacca_log_group01.name
-# }
+output "armageddon_log_group_name" {
+  value = aws_cloudwatch_log_group.lab1b_log_group01.name
+}
