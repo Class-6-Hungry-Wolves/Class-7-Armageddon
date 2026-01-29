c00742bc80be1e8451abbc86156c5ed867eb5d20
NichFos <nicholasdfoster645@gmail.com>
2026-01-29 02:33:10 -0600

    Added providers versions and secret rotation lambda

---
+++ lab-1/lab1c/2-versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  backend "s3" {
+    key    = "lab-1/1c/1232026.tfstate"
+    region = "us-east-1"
+    bucket = "class7-armageddon-tf-bucket"
+  }
+}
