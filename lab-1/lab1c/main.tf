1c338946a167a111922e0ff973cf5e66462b81c3
NichFos <nicholasdfoster645@gmail.com>
2026-02-05 23:12:22 -0600

    Adjusted values for period, threshold, and datapoints_to_alarm. Added explicit treat_missing_data argument.

--- lab-1/lab1c/main.tf
+++ lab-1/lab1c/main.tf
@@ -760,12 +760,14 @@ resource "aws_cloudwatch_log_group" "lab1_log_group01" {
 resource "aws_cloudwatch_metric_alarm" "rds01_db_alarm01" {
   alarm_name          = "${local.project_name_prefix}-db-connection-failure"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
+  evaluation_periods  = 2
   metric_name         = "DBConnectionErrors"
   namespace           = "Lab/RDSApp"
-  period              = 300
+  period              = 60
   statistic           = "Sum"
   threshold           = 3
+  datapoints_to_alarm = 2
+  treat_missing_data  = "notBreaching"       # By default, data is treated as missing which causes alarm to display in the insufficient data state. Setting to "notBreaching" treats missing data as not breaching and alarm will be displayed in "OK" state.
 
   alarm_actions = [aws_sns_topic.armageddon_sns_topic01.arn]
 
