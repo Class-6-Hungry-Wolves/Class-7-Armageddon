1eabe6759bb18ae7ed09bb14aec5e73390e70c73
NichFos <nicholasdfoster645@gmail.com>
2026-02-05 23:13:36 -0600

    Removed old SSH outputs and added outputs for ALB DNS and RDS App FQDN

--- lab-1/lab1c/outputs.tf
+++ lab-1/lab1c/outputs.tf
@@ -33,7 +33,7 @@ output "lab1_ec2_instance_id" {
 }
 
 output "lab1_ec2_private_ip" {
-  value = var.enable_runtime_instance_creation ? "http://${aws_instance.lab1_ec2_instance[0].private_ip}/init" : null
+  value = var.enable_runtime_instance_creation ? aws_instance.lab1_ec2_instance[0].private_ip : null
 }
 output "lab1_rds_endpoint" {
   value = aws_db_instance.lab1-rds01.address
@@ -47,18 +47,16 @@ output "armageddon_log_group_name" {
   value = aws_cloudwatch_log_group.lab1_log_group01.name
 }
 
-# output "fingerprint" {
-#   value = data.aws_key_pair.lab1-key-pair.fingerprint
-# }
-
-# output "name" {
-#   value = data.aws_key_pair.lab1-key-pair.key_name
-# }
-
-# output "id" {
-#   value = data.aws_key_pair.lab1-key-pair.id
-# }
-
 output "packer_builder_instance_profile_name" {
   value = aws_iam_instance_profile.packer_builder_instance_profile.name
+}
+
+
+output "rds_app_alb_dns_name" {
+  value = "http://${aws_lb.app_lb.dns_name}"
+}
+
+
+output "armageddon_rds_app_fqdn" {
+  value = "http://${var.app_subdomain}.${var.root_domain_name}"
 }
